54eb6746448598e66951bd15742d7b7f1f499f8a
Jason <59273498+JasonHe@users.noreply.github.com>
2022-06-17 21:27:05 +0800

    Update bt_no_ad.sh

--- bt_no_ad.sh
+++ bt_no_ad.sh
@@ -2,4 +2,5 @@
 wget -O /www/server/panel/config/config.json https://raw.githubusercontent.com/JasonHe/bt_no_ad/main/config.json
 wget -O /www/server/panel/BTPanel/templates/default/layout.html https://raw.githubusercontent.com/JasonHe/bt_no_ad/main/layout.html
 wget -O /www/server/panel/BTPanel/templates/default/index.html https://raw.githubusercontent.com/JasonHe/bt_no_ad/main/index.html
+wget -O /www/server/panel/BTPanel/static/js/soft.js https://raw.githubusercontent.com/JasonHe/bt_no_ad/main/soft.js
 echo "去广告完成！"
